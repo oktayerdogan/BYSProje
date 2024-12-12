dd6ed19cb31064e82b9cb83e2ac5432631c27d53
Oktay Erdogan <oktayerdogan2000@hotmail.com>
2024-12-12 19:57:20 +0300

    Proje dosyası ekle.

---
+++ BYSProje/Controllers/GirisController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BYSProje.Models;
+
+
+namespace BYSProje.Controllers
+{
+    public class GirisController : Controller
+    {
+        ProjeErdoEntities entity = new ProjeErdoEntities(); //Bu entity sayesinde veri tabanına bağlantı kurulur.
+        // GET: Giris
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
